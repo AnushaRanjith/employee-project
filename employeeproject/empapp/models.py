2da86ee255c7b5da9aeca61a3a7630af79d73cd1
anusha <anusha@gmail.com>
2024-10-11 19:44:17 +0530

    initial commit

---
+++ employeeproject/empapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class Employee(AbstractUser):
+    age=models.IntegerField()
+    phone=models.CharField(max_length=100)
+    department=models.CharField(max_length=100) 
+    designation=models.CharField(max_length=100)
+
